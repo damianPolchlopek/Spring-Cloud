e9daf8822990e527d02b4107f2009968ed73de02
Damian Polchlopek <36795598+damianPolchlopek@users.noreply.github.com>
2026-03-23 17:01:31 +0100

    Add bus refresh

--- PhotoAppApiAcountManagement/src/main/java/org/example/photoappapiacountmanagement/controller/AccountController.java
+++ PhotoAppApiAcountManagement/src/main/java/org/example/photoappapiacountmanagement/controller/AccountController.java
@@ -1,17 +1,22 @@
 package org.example.photoappapiacountmanagement.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @RequestMapping("/account")
 public class AccountController {
 
+    @Value("${app.message:default}")
+    private String message;
 
     @GetMapping("/status/check")
     public String check() {
-        return "Working";
+        return "Working " + message;
     }
 
 }
