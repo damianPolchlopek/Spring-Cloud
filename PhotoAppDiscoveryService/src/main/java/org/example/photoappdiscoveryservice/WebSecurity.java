230698e30e69106854ad82bc066a7fe43877360f
Damian Polchlopek <36795598+damianPolchlopek@users.noreply.github.com>
2026-04-30 15:03:01 +0200

    Add auth to discovery service

---
+++ PhotoAppDiscoveryService/src/main/java/org/example/photoappdiscoveryservice/WebSecurity.java
@@ -0,0 +1,28 @@
+package org.example.photoappdiscoveryservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
